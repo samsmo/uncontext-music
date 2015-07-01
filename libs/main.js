cba21d6378a5b5c44f4ec352010614ba454ccbc4
Sam Smolenski <Sam.Smolenski@thomsonreuters.com>
2015-07-01 16:29:48 -0400

    removing immature refs lul

--- libs/main.js
+++ libs/main.js
@@ -3,14 +3,14 @@ import context from './context';
 
 const twelthRoot = Math.pow(2, (1/12));
 
-// Major Scale, hopefully.
-
+// Major Scale Steps
 let    scale = [0, 2, 4, 5, 7, 9, 11, 12],
 	   socket_ = new WebSocket('ws://duel.uncontext.com:80');
 
 
 // Basic Socket stuff
 // Let notes arbitrarily overlap
+// TODO GAIN FILTER ~ gets loud when data overlaps
 socket_.onmessage = function(data) {
     var parsed = JSON.parse(data.data),
     	steps = scale[Math.floor(parsed.f * scale.length)],
